90ceb7d7e0a95bb023684288ce3f2f96899d42ee
Code-Addiction <t.salisch@t-online.de>
2023-01-24 14:27:32 +0100

    Step 2 finished

--- Project_Raytracing_Renderer/Camera.py
+++ Project_Raytracing_Renderer/Camera.py
@@ -1,5 +1,6 @@
 from Project_Raytracing_Renderer.Ray import Ray
 from Project_Raytracing_Renderer.Vector import Vector
+from Project_Raytracing_Renderer.Image import Image
 
 
 class Camera:
@@ -8,13 +9,24 @@ class Camera:
         self.aspect_ratio = aspect_ratio
         self.viewport_width = viewport_width
         self.viewport_height = self.viewport_width / self.aspect_ratio
-        print('Height: ', self.viewport_height)
-        print('Width: ', self.viewport_width)
         self.origin = origin
         self.horizontal = Vector(self.viewport_width, 0, 0)
         self.vertical = Vector(0, self.viewport_height, 0)
         self.lower_left_corner = (self.origin - self.horizontal / 2 -
                                   self.vertical / 2 - Vector(0, 0, self.focal_length))
 
+    def render(self):
+        width = 400
+        height = int(width // self.aspect_ratio)
+        image = Image(width, height)
+        for j in range(height)[::-1]:
+            for i in range(width):
+                image.update(i, j, self.get_ray(i / (width - 1), j / (height - 1)).color())
+        return image
+
     def get_ray(self, s: float, t: float) -> Ray:
         return Ray(self.origin, self.lower_left_corner + self.horizontal * s + self.vertical * t - self.origin)
+
+
+if __name__ == '__main__':
+    Camera(Vector(0, 0, 0), 1, 16 / 9, 3.555555555555555555555555555556).render().save_image('test.ppm')
--- Project_Raytracing_Renderer/Image.py
+++ Project_Raytracing_Renderer/Image.py
@@ -1,17 +1,16 @@
 import numpy as np
 from Project_Raytracing_Renderer.Vector import Vector
-from Project_Raytracing_Renderer.Camera import Camera
 
 
 class Image:
-    def __init__(self, width: int, height: int) -> None:
+    def __init__(self, width: int, height: int, color: Vector = Vector(0, 0, 0)) -> None:
         self._height = height
         self._width = width
         self._matrix = []
         for i in range(self._width):
             row = []
             for j in range(self._height):
-                row.append(Vector(0, 0, 0))
+                row.append(color)
             self._matrix.append(row)
 
     def to_numpy(self) -> np.ndarray:
@@ -26,24 +25,15 @@ class Image:
 
     def save_image(self, path: str):
         image_str = f'P3\n{self._width} {self._height}\n255'
-        for i in range(self._width):
-            for j in range(self._height):
+        for j in range(self._height):
+            for i in range(self._width):
                 red, green, blue = self._matrix[i][j].to_tuple()
-                image_str = image_str + f'\n{red} {green} {blue}'
+                image_str = image_str + f'\n{int(red)} {int(green)} {int(blue)}'
         with open(path, mode='w+') as f:
             f.write(image_str)
 
-    def render(self, camera):
-        for j in range(self._height)[::-1]:
-            for i in range(self._width):
-                self._matrix[i][j] = camera.get_ray(i / (self._width - 1), j / (self._height - 1)).color()
+    def update(self, i: int, j: int, color: Vector) -> None:
+        self._matrix[i][j] = color
 
     def __str__(self) -> str:
         return str(self._matrix)
-
-
-if __name__ == '__main__':
-    cam = Camera(Vector(0, 0, 0), 1, 16 / 9, 3.555555555556)
-    img = Image(400, int(400 // (16 / 9)))
-    img.render(cam)
-    img.save_image('test.ppm')
