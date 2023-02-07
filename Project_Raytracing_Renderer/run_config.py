ce56e9812b551fd2821a2a86dac82f61f71f9041
Code-Addiction <t.salisch@t-online.de>
2023-02-07 23:28:46 +0100

    Added config files step 10

--- Project_Raytracing_Renderer/run_config.py
+++ Project_Raytracing_Renderer/run_config.py
@@ -78,11 +78,10 @@ def render_config(path: str):
 
 
 def main():
-    # path = input("Please enter path of config file or 'all' for all files in config directory: ")
-    path = 'configs/step9_position5.json'
+    path = input("Please enter path of config file or 'all' for all files in config directory: ")
     if path.lower() == 'all':
         for path in os.listdir("configs/"):
-            if path == 'example.json' or path[:5] != "step8":
+            if path == 'example.json':
                 continue
             render_config("configs/" + path)
     else:
